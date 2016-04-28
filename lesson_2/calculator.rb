03867c33e6ebb8b603e903b617aec1a50bf94c57
Toño Serna <antonelo@gmail.com>
2016-04-29 01:58:05 +0200

    [Lesson 2] Add: rubocop special yml config, Mortgage Calculator, changes in Calculator ( add yml & refactor)

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,31 +1,122 @@
 require 'pry'
+require 'yaml'
 
-puts("Welcome to the ruby calculator!")
-puts("please write the first number")
-number1 = Kernel.gets().chomp().to_f
-puts(number1)
-puts("please write the second number")
-number2 = Kernel.gets().chomp().to_f
-puts(number2)
-puts("Now, which operation you want to do")
-puts <<PARAGRAPH
-1) add
-2) substract
-3) multiply
-4) divide
-PARAGRAPH
+MESSAGES = YAML.load_file("calculator_messages.yml")
 
-operator = Kernel.gets().chomp().to_i
-case operator
-when 1
-  result = number1 + number2
-when 2
-  result = number1 - number2
-when 3
-  result = number1 * number2
-when 4
-  result = number1 / number2
-else
-  puts "please write a number between 1 to 4, please try again"
+number1 ||= 0
+number2 ||= 0
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
-puts(result)
+
+def valid_number?(number)
+  number.to_i.to_s == number
+end
+
+lang_list = <<-LIST
+1)EN
+2)ES
+3)DE
+4)IT
+5)FR
+6)CAT
+you can choose via the name example: en
+LIST
+
+lang = ''
+
+loop do
+  prompt("This is the Calculator scrpit! choose a language:")
+  puts lang_list
+  lang = Kernel.gets().chomp().downcase
+  if %w(en es de it fr cat).include?(lang)
+    prompt(MESSAGES[lang]['welcome'])
+    break
+  else
+    prompt("please try again")
+  end
+end
+
+name = Kernel.gets().chomp()
+prompt("#{name}, #{MESSAGES[lang]['say_hi']}")
+
+loop do
+  number1 = nil
+  loop do
+    prompt(MESSAGES[lang]['first_num'])
+    prompt('please write the first number')
+    check_num = Kernel.gets().chomp()
+    number1 = check_num.to_f
+    valid_number?(check_num) ? break : prompt('Well...That doesn\'t look as a valid number,try again')
+  end
+
+  prompt("That's the first number")
+
+  loop do
+    number2 = nil
+    prompt("please write the second number")
+    check_num = Kernel.gets().chomp()
+    number2 = check_num.to_f
+    valid_number?(check_num) ? break : prompt("well.. That doen't look as valid number, try again")
+  end
+
+  prompt("That's the second number")
+
+  operations = <<-PARAGRAPH
+  Now, wich operation you want to do:
+  1) add
+  2) substract
+  3) multiply
+  4) divide
+  PARAGRAPH
+
+  prompt(operations)
+
+  operator = ''
+
+  loop do
+    operator = Kernel.gets().chomp().to_i
+    %w(1 2 3 4).include?(operator.to_s) ? break : prompt('write a number between 1 to 4, please try again')
+  end
+
+  case operator
+  when 1
+    result = number1 + number2
+  when 2
+    result = number1 - number2
+  when 3
+    result = number1 * number2
+  when 4
+    result = number1 / number2
+  end
+
+  def operation_to_msg(operator)
+    info = case operator
+           when 1
+             'an addition'
+           when 2
+             'a substraction'
+           when 3
+             'a multiplication'
+           when 4
+             'a division'
+           end
+    info
+  end
+
+  prompt("doing #{operation_to_msg(operator)}...")
+
+  puts("This is the result: #{result}")
+  puts "-----"
+  prompt("do you want to continue operating? Y/N")
+  continue = gets.chomp.downcase
+  if continue == 'n'
+    prompt("thanks for using the calculator, bye bye!")
+    break
+  elsif !(continue == 'y')
+    prompt("please write 'y'(as yes) or 'n'(as no)")
+  else
+    next
+  end
+end
---
+++ lesson_2/mortgage_calculator.rb
@@ -0,0 +1,61 @@
+def prompt(text)
+  Kernel.puts("=> #{text}")
+end
+
+def valid_number?(number)
+  number.to_i.to_s == number
+end
+
+def check_floats(number)
+  loop do
+    if valid_number?(number)
+      prompt("please write a number")
+      number = Kernel.gets().chomp()
+    elsif number.to_f() < 0
+      prompt("please write a positive number")
+    else
+      break
+    end
+  end
+end
+
+loop do
+  Kernel.puts("======================================")
+  Kernel.puts("Welcome to the Mortgage Calculator")
+  Kernel.puts("---------------------------------------")
+  Kernel.puts()
+
+  prompt("Write your Loan amount")
+  loan = Kernel.gets().chomp().to_f
+  check_floats(loan)
+
+  prompt("Write your Annual Percentage Rate (APR) for example 5% is 5")
+  apr = Kernel.gets().chomp().to_f
+  check_floats(apr)
+
+  prompt("Write your loan duration *years")
+  years = Kernel.gets().chomp().to_f
+  check_floats(years)
+
+  monthly_interest = (apr / 100) / 12
+  months = years * 12
+
+  prompt("your Loan is #{loan}, your monthly rate is #{format('%0.4f', monthly_interest)} (#{apr} / 100) / 12 )")
+  prompt("and your loan duration in months is #{months} (#{years} years * 12) ")
+
+  monthly_payment = loan * (monthly_interest * (1 + monthly_interest)**months) / ((1 + monthly_interest)**months - 1)
+  
+  prompt(" As we have all variables, now we can calculate our Loan duration in months:")
+   
+  Kernel.puts("monthly payment = #{loan} [#{format('%02.4f', monthly_interest)} (1 + #{format('%02.4f', monthly_interest)}]**#{months} /")
+  Kernel.puts("                  [(1 + #{format('%02.4f', monthly_interest)})**#{months} - 1]")
+  
+  prompt("With a  monthly rate #{format('%02.4f', monthly_interest)}")
+  prompt("Your monthly payment in every of the #{months} months is:")
+  prompt("#{format('%02.2f', monthly_payment)}")
+  prompt("Another calculation?")
+  answer = Kernel.gets().chomp().upcase
+  break unless answer.upcase().start_with?('Y')
+end
+
+prompt("Thanks for using the Mortgage Calculator, bye bye")
