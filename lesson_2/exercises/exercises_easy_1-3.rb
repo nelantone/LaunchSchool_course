87e591c19e0c5e3d9bb4f284d977e28c086f6a72
Toño Serna <antonelo@gmail.com>
2016-05-12 01:38:45 +0200

    first easy excercise 2-3

--- lesson_2/exercises/exercises_easy_1-3.rb
+++ lesson_2/exercises/exercises_easy_1-3.rb
@@ -1,3 +1,5 @@
+### Esay 1 ###
+
 ##1
 numbers = [1, 2, 2, 3]
 numbers.uniq
@@ -100,3 +102,84 @@ flintstones_hash = {}
 flintstones.each_with_index do |name, index|
   flintstones_hash[name] = index
 end
+
+### Esay 2  ###
+## 2.1
+#In this hash of people and their age,
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+#see if there is an age present for "Spot"
+ages.key?('Spot')
+ages.keys.include?('Spot') ## in the solution ther is a mistake, it says only Hahs.include?, we need to specify that we are looking in the keys, otherwise will be always false.
+ages.member?('Spot')
+
+##2.2
+##Add up all of the ages from our current Munster family hash:
+# ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+## Add up?  It means add all ages together!
+ages.values.inject(:+)
+# Inject apply the operator to the accumulator and object parameters of inject.
+
+## 2.3
+##In the age hash:
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+##throw out the really old people (age 100 or older).
+ages.each { |k, _| ages.delete(k) if ages[k] > 100 }
+##or shorter
+ages.keep_if {|_, v| v < 100 }
+
+## 2.4
+##Starting with this string:
+munsters_description = "The Munsters are creepy in a good way."
+##Convert the string in the following ways (code will be executed on original munsters_description above):
+
+##"The munsters are creepy in a good way."
+munsters_description.capitalize!
+##"tHE mUNSTERS ARE CREEPY IN A GOOD WAY."
+munsters_description = munsters_description.upcase.gsub('T','t').gsub('M','m')!
+## or better:
+munsters_description.swapcase!
+##"the munsters are creepy in a good way."
+munsters_description.downcase!
+##"THE MUNSTERS ARE CREEPY IN A GOOD WAY."
+munsters_description.upcase!
+
+## 2.5
+We have most of the Munster family in our age hash:
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10 }
+##add ages for Marilyn and Spot to the existing hash
+additional_ages = { "Marilyn" => 22, "Spot" => 237 }
+##R=
+additional_ages.each_pair {|k,v| ages[k] = v }
+## or shorter
+ages.merge!(additional_ages)
+
+## 2.6
+##Pick out the minimum age from our current Munster family hash:
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+## R=
+ages.values.min
+
+##2.7
+##See if the name "Dino" appears in the string below:
+advice = "Few things in life are as important as house training your pet dinosaur."
+advice.include?('Dino')
+##or
+advice.match?('Dino')
+
+## 2.8
+## In the array:
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+##Find the index of the first name that starts with "Be"
+flinstones.each_with_index {|name,index| puts index if name.match(/Be/) }
+## or shorter
+flintstones.index { |name| name[0, 2] == "Be" }
+
+##2.9-2.10
+##Using array#map!, shorten each of these names to just 3 characters:
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+## R= In one  line.
+flintstones.map! {|short_name| short_name[0,3] }
+
+
