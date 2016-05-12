8780af7fac52a4ac0e8d322228e538bacf060ed9
Toño Serna <antonelo@gmail.com>
2016-05-12 02:51:01 +0200

    first easy excercises 3-3

--- lesson_2/exercises/exercises_easy_1-3.rb
+++ lesson_2/exercises/exercises_easy_1-3.rb
@@ -1,4 +1,4 @@
-### Esay 1 ###
+### Easy 1 ###
 
 ##1
 numbers = [1, 2, 2, 3]
@@ -103,7 +103,7 @@ flintstones.each_with_index do |name, index|
   flintstones_hash[name] = index
 end
 
-### Esay 2  ###
+### Easy 2  ###
 ## 2.1
 #In this hash of people and their age,
 ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
@@ -182,4 +182,69 @@ flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
 ## R= In one  line.
 flintstones.map! {|short_name| short_name[0,3] }
 
+### Easy 3 ###
+
+## 3.1
+##Show an easier way to write this array:
+
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+
+flintstones = %w( Fred Barney Wilma Betty BamBam Pebbels )
+
+## 3.2
+##How can we add the family pet "Dino" to our usual array:
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+flintstones << 'Dino'
+
+## 3.3
+#In the previous exercise we added Dino to our array like this:
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+#flintstones << "Dino"
+##We could have used either Array#concat or Array#push to add Dino to the family.
+
+##How can we add multiple items to our array? (Dino and Hoppy)
+## R= 
+flintstones.push(Dino, Hoppy)
+## to add multiple items we use push otherwise we need to iterate and use <<.
+## we can use concat like this:
+
+flintstones.concat(%w(Dino Hoppy))
+
+## 3.4
+##Shorten this sentence:
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+##...remove everything starting from "house".
+## Review the String#slice! documentation, and use that method to make the return value "Few things in life are as important as ". But leave the advice variable as "house training your pet dinosaur.".
+
+## As a bonus, what happens if you use the String#slice method instead?
+## R=
+advice.slice!('Few things in life are as important as ')
+## or shorter but less clear reading
+advice.slice!(0, advice.index('house'))
+## In hte last method we delimitate with every space from where to where house is 39.
+
+## 3.5
+## Write a one-liner to count the number of lower-case 't' characters in the following string:
+
+statement = "The Flintstones Rock!"
+## R=
+statement.split('').count('t')
+##or
+statement.scan('t').count
+
+
+## 3,6
+##Back in the stone age (before CSS) we used spaces to align things on the screen. If we had a 40 character wide table of Flintstone family members, how could we easily center that title above the table with spaces?
+
+title = "Flintstone Family Members"
+## R=
+one_side_spaces = (40 - title.size) / 2
+title.insert(0," " * one_side_spaces).insert(-1, " " * one_side_spaces)
+## very long
+##just!
+title.center(40)
 
